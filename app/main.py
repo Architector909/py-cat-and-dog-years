aae6d7b2da34b5869de162a39f2a99cbc393b48e
Architector <katusha2105@gmail.com>
2026-06-12 19:04:59 +0300

    2

--- app/main.py
+++ app/main.py
@@ -19,6 +19,6 @@ def get_human_age(cat_age: int, dog_age: int) -> List[int]:
             return 1
         if age < 28:
             return 2
-        return 2  # фикс: дальше НЕ растёт по тестам
+        return 2 + (age - 24) // 5
 
     return [cat_to_human(cat_age), dog_to_human(dog_age)]

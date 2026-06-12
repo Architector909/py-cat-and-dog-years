809a6e296ff1b5c5a66479ad5eae7d72eb8a1cd2
Architector <katusha2105@gmail.com>
2026-06-12 19:17:18 +0300

    4

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    def convert(age: int, step: int) -> int:
+    def cat(age: int) -> int:
         if age < 15:
             return 0
         if age < 24:
             return 1
-        result = 2 + (age - 24) // step
-        return result
+        return 2 + (age - 24) // 4
 
-    return [
-        convert(cat_age, 4),
-        convert(dog_age, 5)
-    ]
+    def dog(age: int) -> int:
+        if age < 15:
+            return 0
+        if age < 24:
+            return 1
+        return 2 + (age - 24) // 5
+
+    return [cat(cat_age), dog(dog_age)]
